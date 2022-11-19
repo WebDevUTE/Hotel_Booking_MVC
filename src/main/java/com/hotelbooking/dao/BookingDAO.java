8a7863efd76e7a360a97bef09bd4f847171e2229
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2022-11-19 22:40:16 +0700

    finish user page

--- src/main/java/com/hotelbooking/dao/BookingDAO.java
+++ src/main/java/com/hotelbooking/dao/BookingDAO.java
@@ -2,9 +2,11 @@ package com.hotelbooking.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
 import com.hotelbooking.JPA.DBUtil;
 import com.hotelbooking.model.Booking;
+import com.hotelbooking.model.Hotel;
 
 public class BookingDAO {
 
@@ -21,5 +23,19 @@ public class BookingDAO {
 		} finally {
 			em.close();
 		}
+	}
+	
+	public Booking getBookingById(int id) {
+		EntityManager em = DBUtil.getFactory().createEntityManager();
+		String query = "SELECT b FROM Booking b WHERE b.bookingId=:id";
+		TypedQuery<Booking> q = em.createQuery(query, Booking.class);
+		q.setParameter("id", id);
+		Booking booking;
+		try {
+			booking = q.getSingleResult();
+		} finally {
+			em.close();
+		}
+		return booking;
 	}
 }
